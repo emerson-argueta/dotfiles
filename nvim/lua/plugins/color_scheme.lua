31493f501ce58011e8c5bcd26ef28c7406012980
emerson.argueta <emerson.argueta@springscs.org>
2024-02-09 05:59:36 -0800

    using dark for now

--- nvim/lua/plugins/color_scheme.lua
+++ nvim/lua/plugins/color_scheme.lua
@@ -89,7 +89,7 @@ return {
       })
 
       vim.cmd.colorscheme('catppuccin')
-      vim.cmd('set background=light')
+      vim.cmd('set background=dark')
       -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
       for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
         vim.api.nvim_set_hl(0, group, {})
